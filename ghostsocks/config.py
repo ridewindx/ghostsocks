828c11d889d25c7d28a07620927d0610959730f4
RideWindX <zhangyongyxbwcc@163.com>
2015-05-14 20:38:56 +0800

    Update config.py

--- ghostsocks/config.py
+++ ghostsocks/config.py
@@ -17,5 +17,10 @@ def parse_args(is_server):
 
     description += '\n\nA fast tunnel proxy that helps you bypass firewalls'
 
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description=description,
+                                     epilog='Online help: github.com/ghostsocks/ghostsocks')
+    proxy_group = parser.add_argument_group('Proxy options')
+    proxy_group.add_argument('-c', '--config', default='./config.py', help='path to config file')
+    general_group = parser.add_argument_group('General options')
     args = parser.parse_args()
