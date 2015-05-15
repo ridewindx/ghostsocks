58c1fa33aecd4b905e747b15326178b6f13c73fd
RideWindX <zhangyongyxbwcc@163.com>
2015-05-15 19:12:58 +0800

    Update log.py

--- ghostsocks/log.py
+++ ghostsocks/log.py
@@ -3,3 +3,7 @@
 
 
 import logging as log
+
+def config_log(filename, level=log.WARNING, filemode='a'):
+    log.basicConfig(filename=filename, filemode=filemode, level=level,
+                    format='%(asctime)s %(levelname)s: %(message)s [tid:%(thread)d][%(filename)s,%(lineno)d]')
